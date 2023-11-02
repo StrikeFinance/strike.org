484c00f173f1b78c1ceb02ef8cdb9c82c7aa5dc5
mafnan2 <afnan.chkhan@gmail.com>
2023-11-03 00:16:41 +0500

    updated queries

--- src/utilities/fetchSanityPosts.js
+++ src/utilities/fetchSanityPosts.js
@@ -1,8 +1,9 @@
+import groq from 'groq';
 import client from './client';
 
 const fetchAllPosts = async () => {
   const posts = await client.fetch(
-    `
+    groq`
     *[
       _type == "post"
       ]|order(date desc){
@@ -21,19 +22,21 @@ const fetchAllPosts = async () => {
 
 const fetchPostDetail = async id => {
   const posts = await client.fetch(
-    `
-    *[
+    groq`*[
       _type == "post" && references(*[_type == "postCategory" && slug.current == "${id}"]._id)
-      ]|order(date desc){
+    ]|order(date desc){
       title,
-      description,
-    publishedAt,
-    featureImage,
-    featured,
-    _createdAt,
-    content,
-      }
-    `
+      subtitle,
+      "imageUrl": mainImage.asset -> url,
+      "slug": "blog/"+slug.current,
+      "textPreview":pt::text(text[0..1]),
+      category->{
+        title,
+        "slug": slug.current
+      },
+      date,
+      "readingTime": round(length(pt::text(text)) / 5 / 180 )
+    }`
   );
   return posts;
 };
